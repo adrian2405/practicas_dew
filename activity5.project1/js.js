14b9e0257889c42392b3a7a88db932caf6ee929b
adrian2405 <riancast24@outlook.com>
2021-06-17 23:10:32 +0100

    final commit

--- activity5.project1/js.js
+++ activity5.project1/js.js
@@ -1,7 +1,7 @@
 class Window {
     constructor(options = {}){
         this.width = options.width || 300;
-        this.height = options.height || 300;
+        this.height = options.height || 320;
         this.posX = options.posX || 150;
         this.posY = options.posY || 500;
         
@@ -11,7 +11,8 @@ class Window {
         this.el.style.top = this.posX + 'px';
         this.el.style.left = this.posY + 'px';
         this.el.style.position = 'absolute';
-        this.el.style.backgroundColor = 'blue';
+        this.el.style.backgroundColor = 'lightblue';
+        this.el.style.border = "thick solid #0000FF";
 
         document.body.appendChild(this.el);
     }
@@ -26,60 +27,73 @@ class Calculator extends Window{
     constructor(options = {}){
         super();
         
-        var content = document.createElement('div');
+        let content = document.createElement('div');
         content.classList.add('container');
 
-        content.innerHTML = `<input class="display col-12 m-2">
+        content.innerHTML = `<input class="display col-12 mt-2 mb-2">
+        <div class="container" id="main">
             <div class="row col-12 mb-3 d-flex">
-            <button class="number-button col-2 m-1 flex-grow-1" data-value="0">0</button>
             <button class="number-button col-2 m-1 flex-grow-1" data-value="1">1</button>
             <button class="number-button col-2 m-1 flex-grow-1" data-value="2">2</button>
             <button class="number-button col-2 m-1 flex-grow-1" data-value="3">3</button>
+            <button class="number-button col-2 m-1 flex-grow-1" data-value="+">+</button>
             </div>
             <div class="row col-12 mb-3">
             <button class="number-button col-2 m-1 flex-grow-1" data-value="4">4</button>
             <button class="number-button col-2 m-1 flex-grow-1" data-value="5">5</button>
             <button class="number-button col-2 m-1 flex-grow-1" data-value="6">6</button>
-            <button class="number-button col-2 m-1 flex-grow-1" data-value="7">7</button>
+            <button class="number-button col-2 m-1 flex-grow-1" data-value="-">-</button>
             </div>
             <div class="row col-12 mb-3">
+            <button class="number-button col-2 m-1 flex-grow-1" data-value="7">7</button>
             <button class="number-button col-2 m-1 flex-grow-1" data-value="8">8</button>
             <button class="number-button col-2 m-1 flex-grow-1" data-value="9">9</button>
-            <button class="number-button col-2 m-1 flex-grow-1" data-value="AC">AC</button>
-            <button class="number-button col-2 m-1 flex-grow-1" data-value="=">=</button>
+            <button class="number-button col-2 m-1 flex-grow-1" data-value="*">*</button>
             </div>
             <div class="row col-12 mb-3">
-            <button class="number-button col-2 m-1 flex-grow-1" data-value="+">+</button>
-            <button class="number-button col-2 m-1 flex-grow-1" data-value="-">-</button>
-            <button class="number-button col-2 m-1 flex-grow-1" data-value="*">*</button>
+            <button class="number-button col-2 m-1 flex-grow-1" data-value="0">0</button>
+            <button class="number-button col-2 m-1 flex-grow-1" data-value=".">.</button>
             <button class="number-button col-2 m-1 flex-grow-1" data-value="/">/</button>
+            <button class="result col-2 m-1 flex-grow-1" data-value="=">=</button>
+            
             </div>
+            <div class="row col-12 mb-3">
+            <button class="clear col-2 m-1 flex-grow-1" data-value="clear">Clear</button>
+            </div>
+
+        </div>
         `;
         this.el.appendChild(content);
 
         this.display = content.querySelector('.display');
         function buttonClick(e) {
-            this.value +=  e.target.dataset.value;
+            this.value += e.target.dataset.value;
         }
         content.querySelectorAll('.number-button').forEach(button =>
             button.addEventListener('click', buttonClick.bind(this.display))
         );
 
-        // this.display = document.createElement('input');
-        // this.buttons = [];
-        // this.buttons.push(document.createElement('button'));
+    
+        let res = document.querySelector(".result");
+        res.addEventListener("click", calculateResult);
 
-        // this.buttons[0].innerText= '0';
 
-        // this.el.appendChild(this.display);
-        // this.el.appendChild(this.buttons[0]);
+        function calculateResult() {
+            let result = eval(document.querySelector('.display').value);
+            document.querySelector('.display').value = result;
+        }
 
-        // function buttonClick(e) {
-        //     this.value +=  e.target.innerText;
-        // }
-        // this.buttons[0].onclick = buttonClick.bind(this.display);
+        let cl = document.querySelector(".clear");
+        cl.addEventListener("click", clean);
+
+        function clean(){
+            document.querySelector('.display').value = '';
+        }
+
+        
     }
 }
 
-let cal = new Calculator;
-cal.appendChild(content);
+    let cal = new Calculator;
+    cal.appendChild(content);
+
